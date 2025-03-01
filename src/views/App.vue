080176d933d84b8a9d5920c34943ce84628d5773
heski0709 <heski0709@gmail.com>
2025-03-01 22:43:48 +0900

    shadcn-ui 사용을 위한 기본 설치

---
+++ src/views/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+</script>
+
+<template>
+    <div class="h-full w-full items-center justify-center flex">
+
+        <Button class="text-white bg-black hover:text-black"> 디스코드 로그인 </Button>
+    </div>
+</template>
